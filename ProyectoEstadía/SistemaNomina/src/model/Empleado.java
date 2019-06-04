92bcea4ad3e6028b5f3409d02926447e2cc12c3d
Gioovanni <51349786+Gioovanni@users.noreply.github.com>
2019-06-03 22:20:47 -0500

    Add files via upload

---
+++ ProyectoEstadía/SistemaNomina/src/model/Empleado.java
@@ -0,0 +1,107 @@
+
+package model;
+
+/**
+ *
+ * @author ramir
+ */
+public class Empleado {
+   
+    private int id;
+    private String numeroUnico;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String fechaIngreso;
+    private double sueldoBase;
+    private int estatus;
+    private Puesto puesto;
+    private Departamento departamento;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumeroUnico() {
+        return numeroUnico;
+    }
+
+    public void setNumeroUnico(String numeroUnico) {
+        this.numeroUnico = numeroUnico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public double getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(double sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public int getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(int estatus) {
+        this.estatus = estatus;
+    }
+
+    public Puesto getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(Puesto puesto) {
+        this.puesto = puesto;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+    
+    
+    
+    @Override
+    public String toString(){
+        return nombre;        
+    }
+}
